5729a4c077627d4b83be93be4473cec1000cfdaf
Petro Yurchyshyn <petro.yurchyshyn.it@gmail.com>
2015-02-26 19:25:49 +0200

    Created sessionFactory

---
+++ Uhmi/src/main/java/healthInsuranceInformation/util/HibernateUtil.java
@@ -0,0 +1,26 @@
+package healthInsuranceInformation.util;
+
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+	private static SessionFactory sessionFactory;
+	
+	private HibernateUtil(){}
+	
+	static{
+		try{
+			Configuration configuration = new Configuration().configure();
+			StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties());
+			sessionFactory = configuration.buildSessionFactory(builder.build());
+		}catch(Throwable e){
+			throw new ExceptionInInitializerError(e);
+		}
+	}
+	
+	public static SessionFactory getSessionFactory(){
+		return sessionFactory;
+	}
+}
