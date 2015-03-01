393327499b783ecfdc28d539cb933c2d57ef2eca
Petro Yurchyshyn <petro.yurchyshyn.it@gmail.com>
2015-03-01 15:31:02 +0200

    added: ProgramDao, FilterDao ProgramFiltersDao classes in Factory

--- Uhmi/src/main/java/healthInsuranceInformation/general/Factory.java
+++ Uhmi/src/main/java/healthInsuranceInformation/general/Factory.java
@@ -1,14 +1,23 @@
 package healthInsuranceInformation.general;
 
 import healthInsuranceInformation.dao.CompanyDao;
+import healthInsuranceInformation.dao.FilterDao;
+import healthInsuranceInformation.dao.ProgramDao;
+import healthInsuranceInformation.dao.ProgramFiltersDao;
 import healthInsuranceInformation.dao.TypeOfClientsDao;
 import healthInsuranceInformation.dao.impl.CompanyDaoImpl;
+import healthInsuranceInformation.dao.impl.FilterDaoImpl;
+import healthInsuranceInformation.dao.impl.ProgramDaoImpl;
+import healthInsuranceInformation.dao.impl.ProgramFiltersDaoImpl;
 import healthInsuranceInformation.dao.impl.TypeOfClientsDaoImpl;
 
 public class Factory {
 	public static Factory instance = new Factory();
 	public CompanyDao companyDao;
 	public TypeOfClientsDao typeOfClientsDao;
+	public ProgramDao programDao;
+	public FilterDao filterDao;
+	public ProgramFiltersDao programFiltersDao; 
 	
 	private Factory(){}
 	
@@ -30,4 +39,24 @@ public class Factory {
 		return typeOfClientsDao;
 	}
 	
+	public ProgramDao getProgramDao(){
+		if(programDao == null){
+			programDao = new ProgramDaoImpl();
+		}
+		return programDao;
+	}
+	
+	public FilterDao getFilterDao(){
+		if(filterDao == null){
+			filterDao = new FilterDaoImpl();
+		}
+		return filterDao;
+	}
+	
+	public ProgramFiltersDao getProgramFiltersDao(){
+		if(programFiltersDao == null){
+			programFiltersDao = new ProgramFiltersDaoImpl();
+		}
+		return programFiltersDao;
+	}
 }
