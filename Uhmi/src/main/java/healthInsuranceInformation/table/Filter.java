19dffed7f9eee8f6d58fee42e32cd626849015a5
Petro Yurchyshyn <petro.yurchyshyn.it@gmail.com>
2015-03-01 15:41:50 +0200

    added class that describes the fields in table "filters" using object oriented approach

---
+++ Uhmi/src/main/java/healthInsuranceInformation/table/Filter.java
@@ -0,0 +1,52 @@
+package healthInsuranceInformation.table;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "filters")
+public class Filter {
+	@Id
+	@Column(name = "filter_id")
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	
+	@Column(name = "name")
+	private String name;
+	
+	@Column(name = "value")
+	private String value;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+	
+	@Override
+    public String toString() {
+       return "Company[ id= " + id + ", name= " + name + ", value= " + value + " ]";
+    }
+}
